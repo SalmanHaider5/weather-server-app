59494e371dabe798a129c4f9025cc0ea328006de
waleed.akram1 <waleed.akram1@tnpak.com.pk>
2019-09-06 12:21:35 +0500

    added auth.js

---
+++ app/middleware/auth.js
@@ -0,0 +1,19 @@
+const apiResponseService = require('../services/apiResponseService');
+
+exports.validateAccessKey = function(req, res, next){
+    try {
+        if(!req.headers['authorization']){
+            throw new Error('Invalid authorization parameter');
+        }
+
+        if(req.headers['authorization'] !== process.env.MICROSERVICE_ACCESS_KEY){
+            throw new Error('Invalid authorization key');
+        }
+        
+        next();
+
+    } catch (error) {
+        let response = apiResponseService.generateResponse(error.code || '401', error.message, '', {} );
+        res.status(401).send(response);
+    }
+}
--- app/routes/api.js
+++ app/routes/api.js
@@ -17,7 +17,4 @@ router.get('/', async function (req, res) {
     }
 });
 
-
-
-
 module.exports = router;
