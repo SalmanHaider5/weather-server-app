6a9b7f26cf61545e10f024833db2a194e189ef86
waleed.akram1 <waleed.akram1@tnpak.com.pk>
2019-09-11 14:49:13 +0500

    validator, customerror and promiseadapter

---
+++ app/adapters/validator.js
@@ -0,0 +1,74 @@
+exports.validationRequired = function (params, required) {
+
+    try {
+        var validationObj = { invalid: false, message: '' };
+        required.some(function (element, index) {
+            validationObj = {};
+            validationObj.invalid = params[element] === undefined || params[element] === "";
+            validationObj.message = element;
+            return validationObj.invalid;
+        });
+    
+        if (validationObj.invalid) {
+            return validationObj;
+        } else {
+            //now check for empty values
+            return required.some(function (element, index) {
+                validationObj = {};
+                validationObj.invalid = params[element].length === 0;
+                validationObj.message = element;
+                return validationObj.invalid;
+            });
+        }
+    } catch (error) {
+        return { invalid: true, message: 'Unknown validation error' };
+    }
+    
+
+}
+
+exports.isSanitisedString = function(str){
+    try {
+        if (! /^[a-zA-Z0-9]+$/.test(str)) {
+            //validation failed
+            return false
+        }else{
+            return true
+        }
+    } catch (error) {
+        return false
+    }
+}
+
+exports.isValidMSISDN = async function(msisdn){
+    try {
+        if(!msisdn){
+            throw new Error('Invalid msisdn');
+        }
+
+        if(!Number.isInteger(Number(msisdn))){
+            throw new Error('Invalid msisdn format');
+        }
+
+        if( !exports.isSanitisedString (msisdn)){
+            throw new Error('Invalid msisdn format');
+        }
+        
+        if(msisdn.substr(0,2) !== '03' ){
+            throw new Error('Invalid msisdn format. Number must start with 03...');
+        }
+
+        if(msisdn.length > 11 || msisdn.length < 9 ){
+            throw new Error('Invalid msisdn format')
+        }
+
+        return new Promise( ( resolve, reject) => {
+            return resolve(true);
+        }) 
+
+    } catch (error) {
+        return new Promise( ( resolve, reject) => {
+            return reject(error);
+        }) 
+    }
+}
